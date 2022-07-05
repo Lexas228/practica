bb6362281d44b314a08d0d0f08ec57b523deac67
d.shishlov <dima.shishlov89@mail.ru>
2022-07-06 01:06:13 +0300

    df

--- practica/src/main/java/ru/vsu/main/Main.java
+++ practica/src/main/java/ru/vsu/main/Main.java
@@ -1,2 +1,186 @@
-package ru.vsu.main;public class Main {
+package ru.vsu.main;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static final String CipherMode = "AES/ECB/PKCS5Padding";
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        ClassLoader classLoader = Main.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("data/dump_002.DMP");
+        byte[] bytes = new byte[inputStream.available()];
+        inputStream.read(bytes);
+        Map<String, Integer> all = new HashMap<>();
+
+
+        for(int i = 0; i < bytes.length; i++){
+            byte[] k = new byte[16];
+            int s = 0;
+            for(int j = i; j < i+16 && j < bytes.length; j++){
+                k[s] = bytes[j];
+                s++;
+            }
+
+            if(s == 16){
+                String l = new String(k);
+                boolean good = true;
+                for(int p = 0; p < l.length(); p++){
+                    if(!Character.isDigit(l.charAt(p))){
+                        good = false;
+                        break;
+                    }
+                }
+                if(good) {
+                    int r = all.getOrDefault(new String(k), 0) + 1;
+                    all.put(new String(k), r);
+                }
+            }
+
+        }
+        all.keySet().forEach(key -> {
+            if(all.get(key) == 2)
+                System.out.println(key);
+        });
+       // map.keySet().stream().filter(key -> map.get(key).equals(2)).forEach(System.out::println);
+        /*String data = readFromInputStream(inputStream);
+        System.out.println(generateKey(128).getEncoded().length);*/
+        //List<String> key = getKey(data);
+        //key.forEach(System.out::println);
+
+
+/*
+
+        InputStream streamOfCipherText = classLoader.getResourceAsStream("data/encr_002");
+        byte[] bytes = new byte[streamOfCipherText.available()];
+        streamOfCipherText.read(bytes);
+        int i = 0;
+        for(String k : key){
+
+            try {
+                byte[] ans = decrypt(bytes, createKeySpec(k));
+                DataBuffer buffer = new DataBufferByte(ans, ans.length);
+                int width = 200;
+                int height = 200;
+
+                WritableRaster raster = Raster.createInterleavedRaster(buffer, width, height, 3 * width, 3, new int[] {0, 1, 2}, (Point)null);
+                ColorModel cm = new ComponentColorModel(ColorModel.getRGBdefault().getColorSpace(), false, true, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+                BufferedImage image = new BufferedImage(cm, raster, true, null);
+
+                ImageIO.write(image, "png", new File("image" + i + ".png"));
+            }catch (Throwable ignored){
+                System.out.println(ignored);
+            }
+            i++;
+        }
+
+*/
+
+    }
+
+
+
+
+    private static String readFromInputStream(InputStream inputStream) throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line);
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
+    private static List<String> getKey(String data){
+        Map<String, Integer> allCount = new HashMap<>();
+
+        for(int i = 0; i < data.length(); i++){
+            StringBuilder builder = new StringBuilder();
+            for(int j = i; j < i + 15 && j < data.length(); j++){
+                char ch  = data.charAt(j);
+                builder.append(ch);
+
+            }
+            if(builder.toString().length() == 15){
+                int count = allCount.getOrDefault(builder.toString(), 0) +1;
+                allCount.put(builder.toString(), count);
+            }
+        }
+        return allCount.keySet().stream().filter(key -> allCount.get(key).equals(2)).collect(Collectors.toList());
+    }
+
+
+    private static byte[] decrypt(byte[] cipherText, SecretKey key) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(cipherText);
+    }
+
+    private static SecretKeySpec createKeySpec(String key){
+        return new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+    }
+
+    private static byte[] hex2byte(String inputString) {
+        if (inputString == null || inputString.length() < 2) {
+            return new byte[0];
+        }
+        inputString = inputString.toLowerCase();
+        int l = inputString.length() / 2;
+        byte[] result = new byte[l];
+        for (int i = 0; i < l; ++i) {
+            String tmp = inputString.substring(2 * i, 2 * i + 2);
+            result[i] = (byte) (Integer.parseInt(tmp, 16) & 0xFF);
+        }
+        return result;
+    }
+
+    public static byte[] getFile() {
+
+        File f = new File("/home/bridgeit/Desktop/Olympics.jpg");
+        InputStream is = null;
+        try {
+            is = new FileInputStream(f);
+        } catch (FileNotFoundException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        }
+        byte[] content = null;
+        try {
+            content = new byte[is.available()];
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        try {
+            is.read(content);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return content;
+    }
+
+    public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(n);
+        SecretKey key = keyGenerator.generateKey();
+        return key;
+    }
 }
--- testingProject/src/Main.java
+++ testingProject/src/Main.java
@@ -1,2 +1,95 @@
-package PACKAGE_NAME;public class Main {
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Stack;
+
+public class Main {
+    private static final String TAG = "TAG";
+    private static final String ans = "LetoCTF{s0m3_ez_Andr01d_r3v}";
+    private static final String last = "_r3v}";
+    private static long next = -1638144296173776243L;
+    private static long s = -1655835832096201751L;
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String l = Long.toBinaryString(next);
+        String k = Long.toBinaryString(s);
+        StringBuilder b = new StringBuilder();
+        for (int p = 0; p < l.length(); p++){
+            if(l.charAt(p) == k.charAt(p)){
+                b.append('0');
+            }else{
+                b.append('1');
+            }
+        }
+        long j = Long.parseLong(b.toString(), 2);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 7; i++){
+            sb.append((char)(j % 256));
+            j >>= 8;
+        }
+        sb.reverse();
+        System.out.println(sb.toString());
+    }
+
+    private static boolean fourthCheck(String str) {
+        long nextLong = new Random(865790124).nextLong();
+        long j = 0;
+        for (byte b : str.getBytes()) {
+            j = (j << 8) + ((long) (b & 255));
+        }
+        long j2 = nextLong ^ j;
+        System.out.println(j2);
+        System.out.println(-1655835832096201751L);
+        if (j2 == -1655835832096201751L) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean firstCheck(String str) {
+        byte[] bytes = "Hackerdom".getBytes();
+        byte[] bArr = {4, 4, 23, 4, 38, 38, 34, 20, 30};
+        byte[] bytes2 = str.getBytes();
+        for (int i = 0; i < bytes.length; i++) {
+            for(byte k = 0; k != (byte)255; k++){
+                byte b = (byte) (bytes[i] ^ k);
+                if(b == bArr[i]){
+                    System.out.println(k);
+                    break;
+                }
+            }
+            bytes[i] = (byte) (bytes[i] ^ bytes2[i]);
+        }
+        return Arrays.equals(bArr, bytes);
+    }
+
+    private static boolean secondCheck(String str) {
+        char[] charArray = "0n5b".toCharArray();
+        char[] charArray2 = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            System.out.print((char)(charArray[i] - i));
+            charArray2[i] = (char) (charArray2[i] + i);
+        }
+        return Arrays.equals(charArray, charArray2);
+    }
+
+    private static boolean thirdCheck(String str) throws NoSuchAlgorithmException {
+        MessageDigest instance = MessageDigest.getInstance("MD5");
+        instance.update(str.getBytes(StandardCharsets.UTF_8));
+        return Arrays.equals(instance.digest(), new byte[]{-78, 45, -66, 74, -120, 86, -12, -104, -20, 125, 14, 102, -65, -6, 105, -53});
+    }
+
+
+
+    private static boolean fifthCheck(String str) {
+        char[] cArr = {'3', 'v', '}', '_', 'r'};
+        for (int i = 0; i < 5; i++) {
+            if (str.charAt(i) != cArr[(i + 3) % 5]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
